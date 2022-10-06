5a451bdc7ccbe47348500d1428386f518ee71478
Moamin <moamin.salamah@gmail.com>
2022-10-06 11:18:07 +0200

    left sidebar

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,9 +1,100 @@
 <template>
-  <div>Side bar</div>
+  <q-drawer
+    show-if-above
+    v-model="leftDrawerOpen"
+    side="left"
+    behavior="desktop"
+    :width="90"
+  >
+    <div class="flex flex-center column q-mb-md">
+      <h6 class="text-weight-bolder">MO</h6>
+    </div>
+    <div class="flex flex-center column q-mt-sm q-mb-md">
+      <q-list>
+        <q-item
+          clickable
+          :active="link === 'user'"
+          @click="link = 'user'"
+          active-class="border"
+        >
+          <q-item-section avatar class="flex flex-center column">
+            <q-icon name="fas fa-user-circle" />
+          </q-item-section>
+        </q-item>
+        <q-item
+          clickable
+          :active="link === 'doc'"
+          @click="link = 'doc'"
+          active-class="border"
+        >
+          <q-item-section avatar class="flex flex-center column">
+            <q-icon name="far fa-file-alt" />
+          </q-item-section>
+        </q-item>
+        <q-item
+          clickable
+          :active="link === 'shopping'"
+          @click="link = 'shopping'"
+          active-class="border"
+        >
+          <q-item-section avatar class="flex flex-center column">
+            <q-icon name="fas fa-shopping-basket" />
+          </q-item-section>
+        </q-item>
+        <q-item
+          clickable
+          :active="link === 'search'"
+          @click="link = 'search'"
+          active-class="border"
+        >
+          <q-item-section avatar class="flex flex-center column">
+            <q-icon name="fas fa-search" />
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </div>
+    <div
+      style="
+        position: absolute;
+        bottom: 20px;
+        margin-left: auto;
+        margin-right: auto;
+        left: 0;
+        right: 0;
+        text-align: center;
+      "
+    >
+      <div class="text-overline">EU</div>
+      <div class="text-overline">US</div>
+    </div>
+  </q-drawer>
 </template>
 
 <script>
-export default {};
+import { ref } from "vue";
+export default {
+  setup() {
+    const leftDrawerOpen = ref(false);
+    return {
+      link: ref("user"),
+      leftDrawerOpen,
+    };
+  },
+};
 </script>
 
-<style></style>
+<style>
+.border {
+  background: #60c470;
+  border-radius: 50%;
+}
+.q-item__section--side {
+  padding-right: 0px !important;
+}
+.q-item__section--avatar {
+  color: black !important;
+}
+.q-item {
+  padding: 16px 0px !important;
+}
+</style>
