bf065ee7c22c44687005c37a1380209d72269b16
Moamin <moamin.salamah@gmail.com>
2022-10-06 14:34:07 +0200

    account dashboard component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,19 @@
 <template>
-  <div class="home"></div>
+  <div class="q-pa-md">
+    <div class="row q-col-gutter-md">
+      <div class="col-4">
+        <AccountDash />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import AccountDash from "../components/AccountDash.vue";
 
 export default {
   name: "HomeView",
-  components: {},
+  components: { AccountDash },
 };
 </script>
